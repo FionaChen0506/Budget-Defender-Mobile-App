b42ab3b72341f46c8699cc16d3514b298077d8cd
FionaChen0506 <fiona.chen9956@gmail.com>
2023-12-05 17:48:14 -0800

    Add Edit Profile function in profile

--- screens/EditProfile.js
+++ screens/EditProfile.js
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { auth, firestore } from '../firebase/firebaseSetup';
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 
-export default function EditProfile() {
+
+const EditProfile = ({navigation}) => {
+  const user = auth.currentUser;
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+
+  useEffect(() => {
+    // Fetch the current user's information when the component mounts
+    const fetchUserData = async () => {
+      try {
+        setEmail(user.email);
+        setUsername(user.displayName);
+
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleSave = async () => {
+    try {
+      // Update the username (displayName) in the Firebase Authentication
+      await updateProfile(user, {
+        displayName: newUsername,
+      });
+
+      // Update the local state
+      setUsername(newUsername);
+  
+      // Reset the newUsername state
+      setNewUsername('');
+      //auth.currentUser.reload()
+      alert('Username updated successfully!');
+      //navigation.goBack();
+      //navigation.goBack({ updatedUsername: newUsername });
+    } catch (error) {
+      console.error('Error updating username:', error);
+    }
+  };
+  
+
   return (
     <View>
-      <Text>EditProfile</Text>
+      <Text>Email: {email}</Text>
+      <Text>Current Username: {auth.currentUser.displayName}</Text>
+      <TextInput
+        placeholder="Enter new username"
+        value={newUsername}
+        onChangeText={(text) => setNewUsername(text)}
+      />
+      <Button title="Save" onPress={handleSave} />
     </View>
-  )
-}
+  );
+};
+
+
+export default EditProfile
+
+const styles = StyleSheet.create({})
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import EditBudgetLimit from '../components/EditBudgetLimit'
 import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
 import { query,collection,where, getDocs } from 'firebase/firestore';
@@ -6,11 +6,26 @@ import { database,auth } from "../firebase/firebaseSetup";
 import { updateInBudgetsDB } from '../firebase/firebaseHelper';
 import { MaterialIcons } from '@expo/vector-icons';
 import Colors from '../styles/Colors';
+import PressableButton from '../components/PressableButton';
+import { onAuthStateChanged } from 'firebase/auth';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const userUid = auth.currentUser.uid;
+  const [user, setUser] = useState(auth.currentUser);
 
+  // Use useEffect to update the user object when it changes
+  useEffect(() => {
+    // Listen for changes in the authentication state
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // Update the local state with the new user object
+      setUser(user);
+    });
+
+    // Clean up the subscription when the component unmounts
+    return () => unsubscribe();
+  }, []);
+  
   // Function to fetch the entryId based on userUid
 const getBudgetEntryId = async (userUid) => {
   try {
@@ -49,10 +64,26 @@ const getBudgetEntryId = async (userUid) => {
     }
   };
 
+  const handleEditProfilePress = () => {
+    // Navigate to the "Edit Profile" screen
+    navigation.navigate('Edit Profile');
+  };
+
   return (
     <View>
-      <Text>{auth.currentUser.email}</Text>
+      <Text> Hi {auth.currentUser.email}</Text>
+      <Text> Hi {user.displayName}</Text>
       <Text>{userUid}</Text>
+
+      <View>
+        <TouchableOpacity 
+        onPress={() => handleEditProfilePress()} 
+        style={styles.EditLimitContainer}>
+          <Text style={styles.EditLimitText}>Edit My Profile</Text>
+          <MaterialIcons name="keyboard-arrow-right" size={26} color="black" />
+        </TouchableOpacity>
+      </View>
+
       <View>
         <TouchableOpacity 
         onPress={() => setIsModalVisible(true)} 
@@ -87,6 +118,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3, 
     shadowRadius: 3, 
     elevation: 4,
+    marginVertical: "1%",
   },
   EditLimitText: {
     color: 'black',
