29a7e619299b6a878e7b8e4f3c459f1bdf9d21d9
Yunke Li <yunkeli.work@gmail.com>
2023-12-06 17:24:35 -0800

    add legend filter to VisitedPlaces

--- screens/VisitedPlaces.js
+++ screens/VisitedPlaces.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, database } from '../firebase/firebaseSetup';
+import { Feather } from '@expo/vector-icons';
 
 // map view dimensions
 const windowWidth = Dimensions.get("window").width;
@@ -19,10 +20,11 @@ export default function VisitedPlaces() {
     const starMarker = require('../images/markers/star.png');
     const redMarker = require('../images/markers/red.png');
 
-
+    const [locationCounts, setLocationCounts] = useState({});
     const [markers, setMarkers] = useState([]);
     const [userUid, setUserUid] = useState(null);
-  
+    const [filter, setFilter] = useState('all');
+
     useEffect(() => {
       // Listen for authentication state to change.
       const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -42,20 +44,66 @@ export default function VisitedPlaces() {
       const q = query(collection(database, "Expenses"), where("user", "==", userId));
       try {
         const querySnapshot = await getDocs(q);
-        const fetchedMarkers = querySnapshot.docs.map(doc => {
-          const location = doc.data().location;
-          return {
-            latitude: location.latitude,
-            longitude: location.longitude,
-            name: location.name,
-            address: location.address
-          };
-        });
-        setMarkers(fetchedMarkers);
-      } catch (error) {
-        console.error("Error fetching Firestore documents for user:", error);
-      }
-    };
+        let fetchedMarkers = [];
+        let counts = {};
+        querySnapshot.docs.forEach(doc => {
+            const location = doc.data().location;
+            const key = location.address; 
+  
+            // Increment count for each location
+            counts[key] = counts[key] ? counts[key] + 1 : 1;
+  
+            fetchedMarkers.push({
+              latitude: location.latitude,
+              longitude: location.longitude,
+              name: location.name,
+              address: location.address
+            });
+          });
+  
+          setMarkers(fetchedMarkers);
+          setLocationCounts(counts);
+        } catch (error) {
+          console.error("Error fetching Firestore documents for user:", error);
+        }
+      };
+    
+
+
+      const getMarkerImage = (locationAddress) => {
+        const count = locationCounts[locationAddress] || 0;
+        if (count >= 5) {
+          return filter === 'all' || filter === 'heart' ? heartMarker : null;
+        } else if (count >= 3) {
+          return filter === 'all' || filter === 'star' ? starMarker : null;
+        }
+        return filter === 'all' ? redMarker : null;
+      };
+
+
+    
+    // Legend component
+    const Legend = () => (
+        <View style={styles.legendContainer}>
+            <View style={styles.titleContainer}>
+                <Feather name="filter" size={20} color="black" />
+                <Text style={styles.legendTitle}> Filter</Text>
+            </View>
+          <TouchableOpacity style={styles.legendItem} onPress={() => setFilter('all')}>
+            <Image source={redMarker} style={styles.legendIcon} />
+            <Text style={styles.legendText}>Show All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.legendItem} onPress={() => setFilter('heart')}>
+            <Image source={heartMarker} style={styles.legendIcon} />
+            <Text style={styles.legendText}>5+ Visits</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.legendItem} onPress={() => setFilter('star')}>
+            <Image source={starMarker} style={styles.legendIcon} />
+            <Text style={styles.legendText}>3+ Visits</Text>
+          </TouchableOpacity>
+        </View>
+      );
+      
   
     return (
       <View style={styles.container}>
@@ -69,21 +117,27 @@ export default function VisitedPlaces() {
             longitudeDelta: LONGITUDE_DELTA,
           }}
         >
-          {markers.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-              title={marker.name}
-            >
+          {markers.map((marker, index) => {
+            const markerImage = getMarkerImage(marker.address);
+            return markerImage && (
+              <Marker
+                key={index}
+                coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                title={marker.name}
+                image={markerImage}
+              >
               <Callout tooltip style={styles.callout}>
-              <View style={styles.calloutView}>
-              <Text style={styles.calloutTitle}>{marker.name}</Text>
-              <Text style={styles.calloutDescription}>{marker.address}</Text>
-            </View>
+                <View style={styles.calloutView}>
+                <Text style={styles.calloutTitle}>{marker.name}</Text>
+                <Text style={styles.calloutDescription}>{marker.address}</Text>
+                </View>
               </Callout>
             </Marker>
-          ))}
+            );
+        })}
         </MapView>
+
+        <Legend />
       </View>
     );
   }
@@ -125,6 +179,43 @@ export default function VisitedPlaces() {
       calloutDescription: {
         fontSize: 14
       },
+      legendContainer: {
+        position: 'absolute', 
+        top: 10, 
+        right: 10,
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5
+      },
+      legendItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
+      },
+      legendIcon: {
+        width: 30,
+        height: 30,
+        marginRight: 5
+      },
+      legendText: {
+        fontSize: 14,
+      },
+        legendTitle: {
+            fontWeight: 'bold',
+            fontSize: 16,
+            marginBottom: 5,
+        },
+        titleContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 5,
+            justifyContent: 'flex-start',
+        },
   });
 
 
